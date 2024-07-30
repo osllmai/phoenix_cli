7078e57e6bffff7bbd391604912499bc758e8e6c
kouhkanamir <Amir@nematifamilyfundation.onmicrosoft.com>
2024-07-30 12:22:17 +0330

    refactor(src): split up list of models and return url model download link

--- src/models_list.cc
+++ src/models_list.cc
@@ -5,18 +5,20 @@
 #include <iostream>
 #include <stdexcept>
 
-std::string list_of_models_available(const std::string &model_name)
-{
+std::map<std::string, std::string> list_of_models_available() {
     std::map<std::string, std::string> models_list;
-    models_list["llama3-8b"] = "https://gpt4all.io/models/gguf/Meta-Llama-3-8B-Instruct.Q4_0.gguf";
-    models_list["llama3-70b"] = "https://gpt4all.io/models/gguf/Meta-Llama-3-8B-Instruct.Q4_0.gguf";
-    models_list["phi"] = "https://gpt4all.io/models/gguf/Meta-Llama-3-8B-Instruct.Q4_0.gguf";
-    try
-    {
+    models_list["llama3-instruct"] = "https://gpt4all.io/models/gguf/Meta-Llama-3-8B-Instruct.Q4_0.gguf";
+    models_list["orca2"] = "https://gpt4all.io/models/gguf/orca-2-7b.Q4_0.gguf";
+    return models_list;
+};
+
+std::string get_url_llm_download(const std::string &model_name) {
+    std::map<std::string, std::string> models_list = list_of_models_available();
+
+    try {
         return models_list.at(model_name);
     }
-    catch (const std::out_of_range &e)
-    {
+    catch (const std::out_of_range &e) {
         std::cerr << "Error: Model name '" << model_name << "' not found." << std::endl;
         return "";
     }
