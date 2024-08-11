8888c89e96fbce2be4e72fb54d6fdce73943d0e0
kouhkanamir <Amir@nematifamilyfundation.onmicrosoft.com>
2024-08-11 17:56:41 +0330

    feat(app): create db

--- app/main.cc
+++ app/main.cc
@@ -1,8 +1,15 @@
 #include <iostream>
 
 #include "commands_list.h"
+#include "database_manager.h"
 
 int main(int argc, char **argv) {
+    // Database management
+    sqlite3 *db;
+    if(sqlite3_open("./build/bin/phoenix.db", &db) == SQLITE_OK) {
+        DatabaseManager::create_tables(db);
+        sqlite3_close(db);
+    }
     show_commands(argc, argv);
     return 0;
 }
