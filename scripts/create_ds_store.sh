dab688697e452e590482c3b5b64f59ce0a298d8e
kouhkanamir <Amir@nematifamilyfundation.onmicrosoft.com>
2024-08-18 21:05:58 +0330

    feat(DMG): add dmg package

--- scripts/create_ds_store.sh
+++ scripts/create_ds_store.sh
@@ -1,19 +1,12 @@
-tell application "Finder"
-    tell disk "@CPACK_PACKAGE_NAME@ Installer"
-        open
-        set current view of container window to icon view
-        set toolbar visible of container window to false
-        set statusbar visible of container window to false
-        set the bounds of container window to {100, 100, 512, 384}
-        set theViewOptions to the icon view options of container window
-        set arrangement of theViewOptions to not arranged
-        set icon size of theViewOptions to 128
-        set background picture of theViewOptions to file ".background:background.png"
-        set position of item "@CPACK_PACKAGE_NAME@.app" of container window to {128, 192}
-        make new alias file at container window to POSIX file "/Applications" with properties {name:"Applications"}
-        set position of item "Applications" of container window to {384, 192}
-        close
-        open
-        update without registering applications
-    end tell
-end tell
+#!/bin/bash
+
+DISK_IMAGE="$1"
+APP_NAME="PhoenixInstaller"
+
+# Check if PROJECT_SOURCE_DIR is set, otherwise set it manually
+if [ -z "$PROJECT_SOURCE_DIR" ]; then
+    PROJECT_SOURCE_DIR="/Users/amir/Workspace/clion/phoenix-cli"
+fi
+
+# Run the AppleScript to set up the .DS_Store file
+osascript "${PROJECT_SOURCE_DIR}/scripts/create_ds_store.applescript.in" "$DISK_IMAGE" "$APP_NAME"
