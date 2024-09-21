e8442ec90be1cac17c73580eec0b6cd294c22d56
kouhkanamir <Amir@nematifamilyfundation.onmicrosoft.com>
2024-09-21 15:49:15 +0330

    get assistant tool by id to routes

--- src/api/routes/assistant_tool.cc
+++ src/api/routes/assistant_tool.cc
@@ -9,20 +9,28 @@ namespace routes {
             return controllers::create_assistant_tool(req);
         });
 
-        CROW_ROUTE(app, "/assistant_tool").methods("GET"_method)([](const crow::request &req){
+        CROW_ROUTE(app, "/assistant_tool").methods("GET"_method)([](const crow::request &req) {
             return controllers::get_assistant_tools(req);
         });
 
-        CROW_ROUTE(app, "/assistant_tool/<int>/<int>").methods("DELETE"_method)([](const crow::request &req, const int &assistant_id, const int &tool_id){
-            return controllers::delete_assistant_tool(req, assistant_id, tool_id);
-        });
+        CROW_ROUTE(app, "/assistant_tool/<int>/<int>").methods("DELETE"_method)(
+                [](const crow::request &req, const int &assistant_id, const int &tool_id) {
+                    return controllers::delete_assistant_tool(req, assistant_id, tool_id);
+                });
 
-        CROW_ROUTE(app, "/assistant_tool/<int>/<int>").methods("PATCH"_method)([](const crow::request &req, const int &assistant_id, const int &tool_id){
-            return controllers::update_assistant_tool(req, assistant_id, tool_id);
-        });
+        CROW_ROUTE(app, "/assistant_tool/<int>/<int>").methods("PATCH"_method)(
+                [](const crow::request &req, const int &assistant_id, const int &tool_id) {
+                    return controllers::update_assistant_tool(req, assistant_id, tool_id);
+                });
 
-        CROW_ROUTE(app, "/assistant_tool/<int>/<int>").methods("GET"_method)([](const crow::request &req, const int &assistant_id, const int &tool_id){
-            return controllers::get_assistant_tool_by_id(req, assistant_id, tool_id);
-        });
+        CROW_ROUTE(app, "/assistant_tool/<int>/<int>").methods("GET"_method)(
+                [](const crow::request &req, const int &assistant_id, const int &tool_id) {
+                    return controllers::get_assistant_tool_by_id(req, assistant_id, tool_id);
+                });
+
+        CROW_ROUTE(app, "/assistant_tool/by-assistant/<int>").methods("GET"_method)(
+                [](const crow::request &req, const int &assistant_id) {
+                    return controllers::get_assistant_tools_by_assistant_id(req, assistant_id);
+                });
     }
 }
